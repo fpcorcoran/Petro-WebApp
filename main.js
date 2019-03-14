17c15c8130f31ba290c909a47f8f2cc14433e5c6
Forrest Corcoran <fpcorcoran17@gmail.com>
2019-03-14 11:56:56 -0400

    Added markers from Precipitation data (32 ports)

--- main.js
+++ main.js
@@ -9,6 +9,34 @@ var map = L.map('mapid', {
 //basemapURL = "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png";
 basemapURL = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
+///Function for digesting Ports_geo.js data and creating workable JSON
+var Markers = (list) => {
+  var LatLong = [];
+  _.each(list, (obj) => {
+    var city = obj.City;
+    var geo = JSON.parse(obj[".geo"]).coordinates;
+    LatLong.push({"City":city,
+                  "Lat":geo[1],
+                  "Lng":geo[0]});
+  });
+  return LatLong;
+};
+
+
+var makeMarkers = (list) => {
+  _.each(list, (obj) => {
+    var cicle = L.circle([obj.Lat, obj.Lng],{
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 200
+    }).bindPopup(obj.City).addTo(map);
+  });
+};
+
+
+var latlong = Markers(Ports);
+makeMarkers(latlong);
 
 baseMap = L.tileLayer(basemapURL, {
   //ext: 'jpg'
